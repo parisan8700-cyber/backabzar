ee3c37a0c63c3334db3562938c1a776e304e2e7a
parisan <parisan0487@gmail.com>
2026-06-29 20:37:47 +0330

    postal

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -3,18 +3,14 @@ const Cart = require("../models/Cart");
 const Product = require("../models/Product");
 
 exports.createOrder = async (userId, items, orderData) => {
+    let totalAmount = 0;
+
     for (const item of items) {
         const product = await Product.findById(item.productId);
 
         if (!product || product.stock < item.quantity) {
             throw new Error(`موجودی ${product?.name || "محصول"} کافی نیست`);
         }
-    }
-
-    let totalAmount = 0;
-
-    for (const item of items) {
-        const product = await Product.findById(item.productId);
 
         const currentPrice =
             item.purchaseType === "installment"
@@ -24,11 +20,33 @@ exports.createOrder = async (userId, items, orderData) => {
         totalAmount += currentPrice * item.quantity;
     }
 
+    let shippingCost = 0;
+
+    switch (orderData.shippingMethod) {
+        case "pickup":
+            shippingCost = 0;
+            break;
+
+        case "post":
+            shippingCost = 100000;
+            break;
+
+        case "express":
+            shippingCost = 200000;
+            break;
+
+        default:
+            throw new Error("روش ارسال نامعتبر است");
+    }
+
     const order = await Order.create({
         ...orderData,
-        amount: totalAmount,
+        amount: totalAmount + shippingCost,
+        shippingCost,
+        shippingMethod: orderData.shippingMethod,
         userId,
     });
+
     return order;
 };
 
